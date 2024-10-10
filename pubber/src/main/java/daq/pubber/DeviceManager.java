1bff79cf08c74bacc07835db56cda85ef56a95c4
erik-radixiot <erik-radixiot@users.noreply.github.com>
2024-10-10 11:41:28 -0600

    MANGO-1546: Simplify DeviceManager

--- pubber/src/main/java/daq/pubber/DeviceManager.java
+++ pubber/src/main/java/daq/pubber/DeviceManager.java
@@ -7,12 +7,6 @@ import daq.pubber.client.GatewayManagerProvider;
 import daq.pubber.client.LocalnetManagerProvider;
 import daq.pubber.client.PointsetManagerProvider;
 import daq.pubber.client.SystemManagerProvider;
-import udmi.schema.Config;
-import udmi.schema.DevicePersistent;
-import udmi.schema.Entry;
-import udmi.schema.Level;
-import udmi.schema.Metadata;
-import udmi.schema.Operation.SystemMode;
 import udmi.schema.PubberConfiguration;
 
 /**
@@ -26,7 +20,6 @@ public class DeviceManager extends ManagerBase implements DeviceManagerProvider 
   private final GatewayManagerProvider gatewayManager;
   private final DiscoveryManagerProvider discoveryManager;
 
-
   /**
    * Create a new instance.
    */
@@ -37,80 +30,6 @@ public class DeviceManager extends ManagerBase implements DeviceManagerProvider 
     localnetManager = new LocalnetManager(host, configuration);
     gatewayManager = new GatewayManager(host, configuration);
     discoveryManager = new DiscoveryManager(host, configuration, this);
-  }
-
-  @Override
-  public void setPersistentData(DevicePersistent persistentData) {
-    systemManager.setPersistentData(persistentData);
-  }
-
-  /**
-   * Set the metadata for this device.
-   */
-  @Override
-  public void setMetadata(Metadata metadata) {
-    pointsetManager.setPointsetModel(metadata.pointset);
-    systemManager.setMetadata(metadata);
-    gatewayManager.setMetadata(metadata);
-  }
-
-  @Override
-  public void activate() {
-    gatewayManager.activate();
-  }
-
-  @Override
-  public void systemLifecycle(SystemMode mode) {
-    systemManager.systemLifecycle(mode);
-  }
-
-  @Override
-  public void maybeRestartSystem() {
-    systemManager.maybeRestartSystem();
-  }
-
-  @Override
-  public void localLog(Entry report) {
-    systemManager.localLog(report);
-  }
-
-  @Override
-  public void localLog(String message, Level trace, String timestamp, String detail) {
-    systemManager.localLog(message, trace, timestamp, detail);
-  }
-
-  @Override
-  public String getTestingTag() {
-    return systemManager.getTestingTag();
-  }
-
-  /**
-   * Update the config of this device.
-   */
-  @Override
-  public void updateConfig(Config config) {
-    pointsetManager.updateConfig(config.pointset);
-    systemManager.updateConfig(config.system, config.timestamp);
-    gatewayManager.updateConfig(config.gateway);
-    discoveryManager.updateConfig(config.discovery);
-    localnetManager.updateConfig(config.localnet);
-  }
-
-  /**
-   * Publish log message for target device.
-   */
-  @Override
-  public void publishLogMessage(Entry logEntry, String targetId) {
-    if (getDeviceId().equals(targetId)) {
-      systemManager.publishLogMessage(logEntry);
-    } else {
-      gatewayManager.publishLogMessage(logEntry, targetId);
-    }
-  }
-
-  @Override
-  public void cloudLog(String message, Level level, String detail) {
-    systemManager.cloudLog(message, level, detail);
   }
 
   @Override
@@ -155,19 +74,19 @@ public class DeviceManager extends ManagerBase implements DeviceManagerProvider 
    */
   @Override
   public void stop() {
-    pointsetManager.stop();
-    localnetManager.stop();
-    gatewayManager.stop();
-    systemManager.stop();
+    getPointsetManager().stop();
+    getLocalnetManager().stop();
+    getGatewayManager().stop();
+    getSystemManager().stop();
   }
 
   /**
    * Set the site model.
    */
   protected void setSiteModel(SiteModel siteModel) {
-    discoveryManager.setSiteModel(siteModel);
-    gatewayManager.setSiteModel(siteModel);
-    localnetManager.setSiteModel(siteModel);
+    getDiscoveryManager().setSiteModel(siteModel);
+    getGatewayManager().setSiteModel(siteModel);
+    getLocalnetManager().setSiteModel(siteModel);
   }
 
 }
