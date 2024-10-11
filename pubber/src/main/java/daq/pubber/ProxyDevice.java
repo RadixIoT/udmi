35045a45678dbca17091870a83016d12073086d1
erik-radixiot <116222103+erik-radixiot@users.noreply.github.com>
2024-10-11 08:22:02 -0600

    MANGO-1546: Run and fix sequencer tests on Mango after integrating Common Library UDMI (#7)
    * Fix initialization issues on Mango
    
    * MANGO-1545, MANGO-1546: Fix update, shutdown and non-implemented methods.
    
    * Fix python dependencies (#973)
    
    * MANGO-1546: Fix gateway_proxy_events test
    
    Run complete test
    
    MANGO-1546: Fix IDE warnings
    
    MANGO-1546: Fix logmap
    
    MANGO-1546: Use default SocketFactory
    
    * MANGO-1546: Simplify DeviceManager
    
    * MANGO-1546: Fix shutdown order
    
    ---------
    
    Co-authored-by: erik-radixiot <erik-radixiot@users.noreply.github.com>
    Co-authored-by: Trevor <grafnu@users.noreply.github.com>

--- pubber/src/main/java/daq/pubber/ProxyDevice.java
+++ pubber/src/main/java/daq/pubber/ProxyDevice.java
@@ -68,11 +68,6 @@ public class ProxyDevice extends ManagerBase implements ProxyDeviceHostProvider 
   }
 
   @Override
-  public void shutdown() {
-    deviceManager.shutdown();
-  }
-
-  @Override
   public void stop() {
     deviceManager.stop();
   }
